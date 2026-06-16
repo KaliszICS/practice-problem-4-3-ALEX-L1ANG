b906636083acc4c47d75da372078b6716ca596d3
ALEX-L1ANG <343293684@tdsb.ca>
2026-06-16 22:41:44 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/*
+File Name: Lesson 4.3 - Do While Loops
+Author: Alex Liang
+Date Created: June 16, 2026
+Date Last Modified June 16, 2026
+*/
+
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -5,14 +14,47 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+
+		Scanner scanner = new Scanner(System.in);
+		int num;
+
+		do {
+        System.out.print("Input a positive integer: ");
+        num = scanner.nextInt();
+    } while (num < 0);
+    
+    System.out.println(num * 2);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner scanner = new Scanner(System.in);
+		String pass;
+		String conf;
+
+		do {
+        System.out.print("Input a password: ");
+        pass = scanner.nextLine();
+        
+        System.out.print("Confirm the password: ");
+        conf = scanner.nextLine();
+    } while (!pass.equals(conf));
+
+	System.out.println("success.");
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner scanner = new Scanner(System.in);
+		int first;
+		int second;
+
+		do {
+        System.out.print("Input an integer: ");
+        first = scanner.nextInt();
+        
+        System.out.print("Input an integer that is a multiple of the first integer: ");
+        second = scanner.nextInt();
+    } while (second % first != 0);
+
+	System.out.println("success.");
 	}
 }
